41220b9b6f281fe5704fdc9c66ab1ecdc22e5118
ASHISH754937 <22bcs11958@cuchd.in>
2025-08-26 09:51:00 +0530

    add refreshToken

--- src/app.js
+++ src/app.js
@@ -10,10 +10,11 @@ app.use(cors({
 }))
 
 
-app.use(express.json({limit:"16kb"}))
-app.use(express.urlencoded({extended:true,limit:"16kb"}))
+app.use(express.json({limit: "16kb"}))
+app.use(express.urlencoded({extended: true, limit: "16kb"}))
 app.use(express.static("public"))
 app.use(cookieParser())
+
 
 
 //routes import
@@ -23,5 +24,5 @@ import userRouter from './routes/user.route.js'
 //routes declaration
 app.use("/api/v1/users",userRouter)
 
-//http://localhost:8000/users/register
+//http://localhost:8000/api/v1/users/register
 export{app}
