ee6adb3779f88beda8eb2a3fdfb0bdb358427ba7
ASHISH754937 <22bcs11958@cuchd.in>
2025-08-26 16:01:07 +0530

    fix: update getCurrentUser response to use ApiError and ensure user ID is safely accessed in updateAccountDetails

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -249,7 +249,7 @@ const changeCurrentPassword = asyncHandler(async(req,res)=>{
 const getCurrentUser= asyncHandler(async(req,res)=>{
      return res
      .status(200)
-     .json(200,req.user,"Current user fetched successfully")
+     .json(new ApiError(200,req.user,"Current user fetched successfully"))
 })
 
 const updateAccountDetails = asyncHandler(async(req,res)=>{
@@ -259,8 +259,8 @@ const updateAccountDetails = asyncHandler(async(req,res)=>{
   {
     throw new ApiError(400,"all fields are required")
   }
-  const user = User.findByIdAndUpdate(
-    req.user._id,
+  const user =await User.findByIdAndUpdate(
+    req.user?._id,
     {
       $set:{
         fullName,
@@ -350,5 +350,5 @@ export {
       updateAccountDetails,
       updateUserAvatar,
       updateUserCoverImage,
-      
+
  };
